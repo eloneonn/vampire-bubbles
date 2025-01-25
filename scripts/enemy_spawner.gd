0d63eca88d08e4f6f8fd1961744eb677fc3a1802
Riddafareous <artem@mgsinvestment.fi>
2025-01-25 14:31:12 +0200

    split bubbles

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -1,11 +1,17 @@
 class_name EnemySpawner extends Node
 
-enum enemy_type {BASIC, FAST, ARMORED}
+enum enemy_type {BASIC, SPLIT, ARMORED}
 
 var basic = preload("res://scenes/bubble.tscn")
 var iron = preload("res://scenes/iron_bubble.tscn")
+<<<<<<< Updated upstream
 var ghost = preload("res://scenes/ghost_bubble.tscn")
+=======
+var split = preload("res://scenes/splitting_buble.tscn")
+var baby = preload("res://scenes/baby_bubble.tscn")
+
+>>>>>>> Stashed changes
 func spawn_enemy():
-	var instance = iron.instantiate()
+	var instance = split.instantiate()
 	instance.position = get_parent().global_position
 	get_tree().root.add_child(instance)
