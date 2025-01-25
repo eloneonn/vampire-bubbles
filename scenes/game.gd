2f73fdc4d9b8fa5b8ba4f3a87dd056ed57dfd176
eloneonn <63492799+eloneonn@users.noreply.github.com>
2025-01-25 11:27:13 +0200

    pause menu and 2p

--- scenes/game.gd
+++ scenes/game.gd
@@ -2,20 +2,22 @@ extends Node2D
 
 @onready var game_over_screen: CanvasLayer = $GameOverScreen
 @onready var victory_screen: CanvasLayer = $VictoryScreen
+@onready var main_menu: CanvasLayer = $MainMenu
+@onready var check_button: CheckButton = $MainMenu/MarginContainer/CenterContainer/VBoxContainer/CheckButton
+@onready var pause_menu: CanvasLayer = $PauseMenu
 
 func _ready() -> void:
-	get_tree().paused = false
-	GameManager.start_game()
+	main_menu.visible = true
+	get_tree().paused = true
 	GameManager.game_ended.connect(on_game_end)
 	GameManager.victory.connect(on_victory)
+	
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		pause_game()
 
 func _on_restart_button_pressed() -> void:
-	for node in get_tree().get_nodes_in_group("enemies"):
-		node.queue_free()
-
-	get_tree().paused = false
-	game_over_screen.visible = false
-	get_tree().reload_current_scene()
+	reload_game()
 
 func on_game_end() -> void:
 	game_over_screen.visible = true
@@ -24,3 +26,31 @@ func on_game_end() -> void:
 func on_victory() -> void:
 	victory_screen.visible = true
 	get_tree().paused = true
+
+func pause_game(): 
+	pause_menu.visible = true
+	get_tree().paused = true
+	
+func reload_game():
+	for node in get_tree().get_nodes_in_group("enemies"):
+		node.queue_free()
+	
+	get_tree().paused = false
+	game_over_screen.visible = false
+	get_tree().reload_current_scene()
+
+func _on_start_game_pressed() -> void:
+	get_tree().paused = false
+	main_menu.visible = false
+	GameManager.start_game()
+
+func _on_check_button_toggled(toggled_on: bool) -> void:
+	GameManager.two_player_game = toggled_on
+	check_button.button_pressed = toggled_on
+
+func _on_continue_pressed() -> void:
+	pause_menu.visible = false
+	get_tree().paused = false
+
+func _on_back_to_menu_pressed() -> void:
+	reload_game()
--- scripts/enemy_1.gd
+++ scripts/enemy_1.gd
@@ -8,6 +8,7 @@ extends CharacterBody2D
 
 @export var spawn_interval: float = 1.0
 var angle = 0.0
+var SPEED = 300
 
 func _ready() -> void:
 	timer.wait_time = spawn_interval
@@ -19,8 +20,16 @@ func spawn():
 	if enemy_spawner:
 		enemy_spawner.spawn_enemy()
 
-func _process(delta):
-	if player:
+func _physics_process(delta):
+	if GameManager.two_player_game:
+		var mouse_pos = get_global_mouse_position()
+		var direction = (mouse_pos - global_position).normalized()
+		velocity = direction * SPEED
+		
+		move_and_slide()
+		
+		global_position = get_global_mouse_position()
+	elif player:
 		var player_node = get_node(player)
 		angle += orbit_speed * delta
 		var offset = Vector2(cos(angle), sin(angle)) * orbit_radius
