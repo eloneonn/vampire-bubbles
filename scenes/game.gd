dbe9ffbf05893f6ec9c0b6205242e76d03aedb36
eloneonn <63492799+eloneonn@users.noreply.github.com>
2025-01-25 10:49:32 +0200

    victory screen

--- scenes/game.gd
+++ scenes/game.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
 @onready var game_over_screen: CanvasLayer = $GameOverScreen
+@onready var victory_screen: CanvasLayer = $VictoryScreen
 
 func _ready() -> void:
 	get_tree().paused = false
 	GameManager.start_game()
 	GameManager.game_ended.connect(on_game_end)
+	GameManager.victory.connect(on_victory)
 
 func _on_restart_button_pressed() -> void:
 	for node in get_tree().get_nodes_in_group("enemies"):
@@ -16,6 +18,9 @@ func _on_restart_button_pressed() -> void:
 	get_tree().reload_current_scene()
 
 func on_game_end() -> void:
-	
 	game_over_screen.visible = true
+	get_tree().paused = true
+
+func on_victory() -> void:
+	victory_screen.visible = true
 	get_tree().paused = true
